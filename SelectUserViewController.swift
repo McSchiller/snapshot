cca3403a1d354b42199fba1fac595ded83524794
Andreas Schiller <andreasschiller@Andreass-MBP.fritz.box>
2017-02-11 03:04:34 +0100

    Fotos werden hochgeladen

---
+++ SelectUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectUserViewController.swift
+//  Snapshot
+//
+//  Created by Andreas Schiller on 11.02.17.
+//  Copyright © 2017 Schiller. All rights reserved.
+//
+
+import UIKit
+
+class SelectUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
--- Snapshot/PicturesViewController.swift
+++ Snapshot/PicturesViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseStorage
 
-class PicturesViewController: UIViewController, UITextFieldDelegate {
+class PicturesViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
     
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         descriptionTextField.delegate = self
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -27,10 +32,57 @@ class PicturesViewController: UIViewController, UITextFieldDelegate {
         descriptionTextField.resignFirstResponder()
         return true
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
+   
+    @IBAction func libaryTabbed(_ sender: Any) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     @IBAction func cameraTabbed(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     @IBAction func nextTabbed(_ sender: Any) {
+        
+        nextButton.isEnabled = false
+        nextButton.setTitle("Senden wird vorbereitet...", for: .normal)
+        
+        let imagesFolder = FIRStorage.storage().reference().child("images")
+        
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.1)!
+        
+        imagesFolder.child("\(NSUUID().uuidString).jpg").put(imageData, metadata: nil) { (metadata, error) in
+            print("*** We tried to upload!")
+            if error != nil {
+                print("*** We had an error:\(error)")
+            } else {
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
+                  print(metadata?.downloadURL())
+                
+                //  self.performSegue(withIdentifier: "selectUsersegue", sender: metadata!.downloadURL()!.absoluteString)
+            }
+        }
+        
+        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+    }
 }
